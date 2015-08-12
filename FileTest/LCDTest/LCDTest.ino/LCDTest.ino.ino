30865b867fd276ac341d374fbeaf556a13c3ab12
JeerawatIT_Innovation <gukaihero@gmail.com>
2015-08-12 17:15:31 +0700

    ลบส่วนที่ไม่สำคัญ

--- FileTest/LCDTest/LCDTest.ino/LCDTest.ino.ino
+++ FileTest/LCDTest/LCDTest.ino/LCDTest.ino.ino
@@ -1,16 +1,15 @@
 #include<Wire.h>
-#include<LCD.h>
 #include<LiquidCrystal_I2C.h>
 
-#define I2C_ADDR 0x27 // กำหนดตำแหน่ง Address ของ I2C
+//#define I2C_ADDR 0x27 // กำหนดตำแหน่ง Address ของ I2C
 //#define I2C_ADDR 0x3F // ในบางกรณีที่ Address ของ I2C เป็น 0x3F
 #define BACKLIGHT_PIN 3 
 
-LiquidCrystal_I2C lcd(I2C_ADDR,2,1,0,4,5,6,7,3,POSITIVE); // กำหนด Address ให้กับ LCD
+LiquidCrystal_I2C lcd(0x27,16,2); // กำหนด Address ให้กับ LCD
 
 void setup() {
   // lcd.begin (20,4);
-  lcd.begin (16,2);
+  lcd.begin ();
 
   /* byte customChar0[8]={
      0b00000,
